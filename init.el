82214da6010647c24cdaeca5738543098b33b02a
Smertos <misha.smert@gmail.com>
2018-08-04 02:29:25 +0500

    First publication

---
+++ init.el
@@ -0,0 +1,27 @@
+;;; init.el --- init.org bootstrapper
+
+;;; Commentary:
+;; This is bootstrapper of my org config
+
+;;; Code:
+(require 'package)
+
+;; Setup package repos
+(setq package-archives '(("org"       . "http://orgmode.org/elpa/")
+                         ("gnu"       . "http://elpa.gnu.org/packages/")
+                         ("melpa"     . "http://melpa.org/packages/")))
+(package-initialize)
+
+;; Bootstrap use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Path to config.org
+(defconst config-path "~/.emacs.d/config.org")
+
+;; Load the config itself
+(when (file-readable-p config-path)
+  (org-babel-load-file (expand-file-name config-path)))
+
+;;; init.el ends here
